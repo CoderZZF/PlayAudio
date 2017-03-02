01ea1c8e3722662817b6182f96eb128005e9606c
zhangzhifu <595373131@qq.com>
2017-03-02 21:48:47 +0800

    封装播放音效工具类

--- 音效的播放/音效的播放/ViewController.m
+++ 音效的播放/音效的播放/ViewController.m
@@ -8,9 +8,12 @@
 
 #import "ViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import "XMGAudioTool.h"
 
 @interface ViewController ()
 @property (nonatomic, assign) SystemSoundID soundID;
+/** 存放音效文件 */
+@property (nonatomic, strong) NSMutableDictionary *soundIDs;
 @end
 
 @implementation ViewController
@@ -18,16 +21,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+}
+
+- (IBAction)buyao {
+//    [self playSoundWithSoundName:@"buyao.wav"];
+    [XMGAudioTool playSoundWithSoundName:@"buyao.wav"];
+}
+
+- (IBAction)dawang {
+//    [self playSoundWithSoundName:@"m_17.wav"];
+    [XMGAudioTool playSoundWithSoundName:@"m_17.wav"];
+}
+
+- (IBAction)xiaowang {
+//    [self playSoundWithSoundName:@"m_16.wav"];
+    [XMGAudioTool playSoundWithSoundName:@"m_16.wav"];
+}
+
+/*
+- (void)playSoundWithSoundName:(NSString *)soundName {
+    // 1. 定义systemSoundID
+    SystemSoundID soundID = 0;
     
+    // 2. 从字典中取对应的soundID,如果取出是nil,表示之前没有存在字典中
+    soundID = [self.soundIDs[soundName] unsignedIntValue];
+    if (soundID == 0) {
+        CFURLRef url = (__bridge CFURLRef)[[NSBundle mainBundle] URLForResource:soundName withExtension:nil];
+        AudioServicesCreateSystemSoundID(url, &soundID);
+        
+        // 将soundID存入字典中
+        [self.soundIDs setObject:@(soundID) forKey:soundName];
+    }
+    
+    // 3. 播放音效
+    AudioServicesPlayAlertSound(soundID);
 }
+*/
 
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    // 播放音效
-//    AudioServicesPlaySystemSound(soundID);
-    AudioServicesPlayAlertSound(self.soundID);
-}
-
+#pragma mark - 懒加载
 - (SystemSoundID)soundID {
     if (_soundID == 0) {
         // 2. 根据音效文件生成systemSoundID
@@ -36,5 +67,14 @@
     }
     return _soundID;
 }
+
+- (NSMutableDictionary *)soundIDs {
+    if (_soundIDs == nil) {
+        _soundIDs = [NSMutableDictionary dictionary];
+    }
+    return _soundIDs;
+}
+
+
 
 @end
